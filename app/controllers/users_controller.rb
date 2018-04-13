763d8145563526691d1fd0b77b8df76a6946f3a8
Nolan <nolanehs@gmail.com>
2018-04-13 18:20:56 -0400

    .

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,26 +2,26 @@ class UsersController < ApplicationController
 
 
 def create
-  # @user = User.new(user_params)
-  # @user.save
-  # redirect to user_path(current_user)
+#   # @user = User.new(user_params)
+#   # @user.save
+#   # redirect to user_path(current_user)
 end
-
-
-def show
-  # @user= @user.find(params[:id])
-end
-
-
-def destroy
-  reset_session
-  redirect_to root_path
-end
-
-private
-
-def user_params
-  params.require(:user).permit(:first_name, :last_name, :location, :profession)
-end
+#
+#
+# def show
+#   # @user= @user.find(params[:id])
+# end
+#
+#
+# def destroy
+#   reset_session
+#   redirect_to root_path
+# end
+#
+# private
+#
+# def user_params
+#   params.require(:user).permit(:first_name, :last_name, :location, :profession)
+# end
 
 end
