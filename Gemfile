e7bab9244e61ff87541a37894ba80872f71d60b5
Nolan <nolanehs@gmail.com>
2018-04-13 18:13:23 -0400

    Added Rspec

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ git_source(:github) do |repo_name|
   repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
   "https://github.com/#{repo_name}.git"
 end
+gem 'rspec-rails'
 gem 'omniauth'
 gem 'pry'
 gem 'dotenv-rails'
@@ -41,11 +42,13 @@ group :development, :test do
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '~> 2.13'
   gem 'selenium-webdriver'
+  gem 'rspec-rails'
 end
 
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
   gem 'web-console', '>= 3.3.0'
+  gem 'rspec-rails'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,22 +2,14 @@ class UsersController < ApplicationController
 
 
 def create
-
-  @user = User.new
-  @user.first_name = params[:first_name]
-  @user.last_name = params[:last_name]
-  @user.location = params[:location]
-  @user.profession = params[:profession]
-  
-  if @user.save
-    session[:user_id] = @user.id
-    redirect to user_path(@user)
-  end
+  # @user = User.new(user_params)
+  # @user.save
+  # redirect to user_path(current_user)
 end
 
 
 def show
-  @user= User.find(params[:id])
+  # @user= @user.find(params[:id])
 end
 
 
@@ -25,4 +17,11 @@ def destroy
   reset_session
   redirect_to root_path
 end
+
+private
+
+def user_params
+  params.require(:user).permit(:first_name, :last_name, :location, :profession)
+end
+
 end
