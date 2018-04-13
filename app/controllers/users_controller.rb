addb03b2fb37b9b97aea779242137f6b8b04e329
Nolan <nolanehs@gmail.com>
2018-04-13 16:04:26 -0400

    Added fields to forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,26 @@
 class UsersController < ApplicationController
 
-def show
+
+def create
+
+  @user = User.new
+  @user.first_name = params[:first_name]
+  @user.last_name = params[:last_name]
+  @user.location = params[:location]
+  @user.profession = params[:profession]
+  
+  if @user.save
+    session[:user_id] = @user.id
+    redirect to user_path(@user)
+  end
 end
+
+
+def show
+  @user= User.find(params[:id])
+end
+
+
 def destroy
   reset_session
   redirect_to root_path
