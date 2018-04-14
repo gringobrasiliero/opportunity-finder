a3ab4e702ba398e750ad9da3897f65ca659ccb6d
Nolan <nolanehs@gmail.com>
2018-04-14 08:00:01 -0400

    added opportunities pages index, new, show

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -8,6 +8,11 @@ def create
   redirect_to @profile
 end
 
+def index
+  @user = current_user
+  @profile = @user.profile
+end
+
 def show
   @user = current_user
   @profile = @user.profile
@@ -16,7 +21,7 @@ end
 private
 
 def profile_params
-   params.require(:profile).permit(:first_name, :last_name, :location, :profession)
+   params.require(:profile).permit(:user_id, :first_name, :last_name, :location, :profession)
  end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
       end
 
   get 'sessions/new'
-
+resources :profiles
+resources :opportunities
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  root "welcome#home"
