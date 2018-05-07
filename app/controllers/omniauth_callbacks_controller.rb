935b0fc359f468aa6240b4d6fbcaf92654434a50
Nolan Byrnes <nolanehs@gmail.com>
2018-05-07 16:28:49 -0400

    refactoring. Don't need profiles#index

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,20 +1,5 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
-
-#   def linkedin
-#     # You need to implement the method below in your model (e.g. app/models/user.rb)
-#     @user = User.from_omniauth(request.env["omniauth.auth"])
-# @profile = @user.profile.from_omniauth(request.env["omniauth.auth"])
-#
-#     if @user.persisted?
-#       sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
-#       set_flash_message(:notice, :success, kind: "Linkedin") if is_navigational_format?
-#     else
-#       session["devise.linkedin_data"] = request.env["omniauth.auth"]
-#       redirect_to new_user_registration_url
-#     end
-#   end
-
   def failure
     redirect_to root_path
   end
