022657e3862ad39e6baf5489d6c9afae67739c4b
Nolan Byrnes <nolanehs@gmail.com>
2018-05-08 05:56:39 -0400

    Deleted unneeded code. fixed application forms. refactoring.

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,11 +1,3 @@
 module ProfilesHelper
 
-  # def profiles_attributes=(profiles_attributes)
-  #   profile_attributes.values.each do |profile_attribute|
-  #   profile = Profile.find_or_create_by(profile_attribute)
-  #   self.profiles.update(profile_attribute)
-  # end
-  # end
-
-
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,12 +7,5 @@ module UsersHelper
   end
   end
 
-  # def profile_attributes=(profiles_attributes)
-  #   profile_attributes.values.each do |profile_attribute|
-  #   profile = Profile.find_or_create_by(profile_attribute)
-  #   self.profile.update(profile_attribute)
-  # end
-  # end
-
 
 end
