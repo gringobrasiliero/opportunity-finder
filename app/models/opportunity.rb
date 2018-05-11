9d0b77d0133a1ab8b6b955f083dee5a17893e341
Nolan Byrnes <nolanehs@gmail.com>
2018-05-11 10:56:40 -0400

    Formatting changes

--- app/models/opportunity.rb
+++ app/models/opportunity.rb
@@ -3,7 +3,7 @@ class Opportunity < ApplicationRecord
   validates :description, presence:true
 
   belongs_to :user
-  has_many :applications
+  has_many :applications, :dependent => :destroy
   has_many :users, :through => :applications
 
 end
