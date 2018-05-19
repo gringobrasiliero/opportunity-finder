2510f878f21852f178266e778dea2defc10bd692
Nolan Byrnes <nolanehs@gmail.com>
2018-05-19 06:02:07 -0400

    Fixed Validation Messages. Added URL validator for pic_url
    Beautified code as well.

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,9 +1,10 @@
 class Profile < ApplicationRecord
-validates :first_name, :presence => { message: "First name required"}
-validates :last_name, :presence => { message: "Last name required"}
-validates :location, :presence => { message: "What area do you live in?"}
-validates :picture_url, :presence => { message: "We need a picture URL to identify your name to a face."}
-validates :profession, :presence => { message: "What is your profession?"}
+validates :first_name, :presence => { message: "required"}
+validates :last_name, :presence => { message: "required"}
+validates :location, :presence => { message: "required"}
+validates :picture_url, :presence => { message: "required"}
+validates :profession, :presence => { message: "required"}
+ validates_format_of :picture_url, :with => URI::regexp(%w(http https))
 
   belongs_to :user
 
