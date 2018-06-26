491ab17c62f0b3049f6097b0f6e5325a0c21ffaf
Nolan Byrnes <nolanehs@gmail.com>
2018-06-26 06:12:30 -0400

    Appending Entire Application to opp/show page
    Took out Application in opp/show page to stop repeated content

--- app/assets/javascripts/applications.js
+++ app/assets/javascripts/applications.js
@@ -20,14 +20,23 @@
 $(".load_application").on("click", function(e){
   var id = $(this).data("id");
   $.get("/applications/" + id + ".json")
-  .success(function(data) {
-  console.log(data)
+  .success(function(json) {
+var $div = $("#applicant-" + id)
+$div.html("") //empties the div
+  console.log(json)
+$("#applicant-" + id).append('<h2>Application</h2>')
+$("#applicant-" + id).append('<div class="field"><h3>Qualified for Position: ' + json["qualified"] + '</h3></div>')
+$("#applicant-" + id).append('<div class="field"><h3>Reliable Transportation: ' + json["transportation"] + '</h3></div>')
+$("#applicant-" + id).append('<div class="field"><h3>Criminal Record: ' + json["criminal_record"] + '</h3></div>')
+$("#applicant-" + id).append('<div class="field"><h3>Description of Criminal Record: ' + json["description_of_criminal_record"] + '</h3></div>')
+$("#applicant-" + id).append('<div class="field"><h3>Legal to work in USA: ' + json["legal"] + '</h3></div>')
+$("#applicant-" + id).append('<div class="field"><h3>Month Commitment: ' + json["month_commitment"] + '</h3></div>')
+$("#applicant-" + id).append('<div class="field"><h3>Reason for Interest: ' + json["reason_for_interest"] + '</h3></div>')
 
 });
+
 e.preventDefault();
 });
-
-
 
 
 
