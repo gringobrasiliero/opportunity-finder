864483a6eb63802f1be48e826a918f303bec96ce
Nolan Byrnes <nolanehs@gmail.com>
2018-06-27 05:33:00 -0400

    Fixed specs

--- spec/models/opportunity_spec.rb
+++ spec/models/opportunity_spec.rb
@@ -26,30 +26,32 @@ RSpec.describe Opportunity, type: :model do
     )
   }
 
+  let(:application) {
+    Application.create(
+    :id => 1,
+    :user_id => user.id,
+    :opportunity_id => opportunity.id,
+    :qualified => true,
+    :legal => false,
+    :month_commitment => 6,
+    :reason_for_interest => "Defeat him",
+    :criminal_record => true,
+    :description_of_criminal_record => "N/A",
+    :transportation => true
+
+    )
+  }
+
+
 it "belongs to user" do
   expect(opportunity.user).to eq(user)
 end
 
 it "has many applications" do
-  first_app = Application.create( :user_id => 1, :qualified => true, :legal => false, :month_commitment => 6, :reason_for_interest => "Defeat him", :transportation => true, :criminal_record => false, :description_of_criminal_record => false)
-  second_app = Application.create( :user_id => 2, :qualified => false, :legal => true, :month_commitment => 1, :reason_for_interest => "Defeat him fast", :transportation => true, :criminal_record => false, :description_of_criminal_record => false)
+  first_app = Application.create( :opportunity_id => opportunity.id, :user_id => user.id, :qualified => true, :legal => false, :month_commitment => 6, :reason_for_interest => "Defeat him", :transportation => true, :criminal_record => false, :description_of_criminal_record => false)
+  second_app = Application.create( :opportunity_id => opportunity.id, :user_id => user.id, :qualified => false, :legal => true, :month_commitment => 1, :reason_for_interest => "Defeat him fast", :transportation => true, :criminal_record => false, :description_of_criminal_record => false)
    expect(opportunity.applications.first).to eq(first_app)
   expect(opportunity.applications.last).to eq(second_app)
 end
-
-
-
-
-
-it "has many users through applications" do
-
-
-opportunity.users << [user, user_one]
-expect(opportunity.users.first).to eq(user)
-expect(opportunity.users.last).to eq(user_one)
-
-end
-
-
 
 end
