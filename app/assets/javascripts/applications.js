4abeed1b787df8a919cdae2bbe46a7ff410e7927
Nolan Byrnes <nolanehs@gmail.com>
2018-06-27 05:31:17 -0400

    Refactored application.js

--- app/assets/javascripts/applications.js
+++ app/assets/javascripts/applications.js
@@ -6,12 +6,13 @@
     $.get("/opportunities/" + id + ".json")
     .success(function(data) {
       // console.log(data["opportunity"]["applications"])
-
+      var $div = $("#oppApps-" + id)
+      $div.html("") //empties the div
       var oppApps = data["opportunity"]["applications"]
 
       oppApps.forEach(function(element) {
         console.log(element)
-          $("#oppApps-" + id).append(element["legal"])
+          $div.append(element["legal"])
       })
 });
   e.preventDefault();
@@ -25,19 +26,30 @@ var $div = $("#applicant-" + id)
 $div.html("") //empties the div
   console.log(json)
 $("#applicant-" + id).append('<h2>Application</h2>')
-$("#applicant-" + id).append('<div class="field"><h3>Qualified for Position: ' + json["qualified"] + '</h3></div>')
-$("#applicant-" + id).append('<div class="field"><h3>Reliable Transportation: ' + json["transportation"] + '</h3></div>')
-$("#applicant-" + id).append('<div class="field"><h3>Criminal Record: ' + json["criminal_record"] + '</h3></div>')
-$("#applicant-" + id).append('<div class="field"><h3>Description of Criminal Record: ' + json["description_of_criminal_record"] + '</h3></div>')
-$("#applicant-" + id).append('<div class="field"><h3>Legal to work in USA: ' + json["legal"] + '</h3></div>')
-$("#applicant-" + id).append('<div class="field"><h3>Month Commitment: ' + json["month_commitment"] + '</h3></div>')
-$("#applicant-" + id).append('<div class="field"><h3>Reason for Interest: ' + json["reason_for_interest"] + '</h3></div>')
-
+$div.append('<div class="field"><h3>Qualified for Position: ' + json["qualified"] + '</h3></div>')
+$div.append('<div class="field"><h3>Reliable Transportation: ' + json["transportation"] + '</h3></div>')
+$div.append('<div class="field"><h3>Criminal Record: ' + json["criminal_record"] + '</h3></div>')
+$div.append('<div class="field"><h3>Description of Criminal Record: ' + json["description_of_criminal_record"] + '</h3></div>')
+$div.append('<div class="field"><h3>Legal to work in USA: ' + json["legal"] + '</h3></div>')
+$div.append('<div class="field"><h3>Month Commitment: ' + json["month_commitment"] + '</h3></div>')
+$div.append('<div class="field"><h3>Reason for Interest: ' + json["reason_for_interest"] + '</h3></div>')
+// $div.append('<a href="' + id + '" class= "hide_application" data-id="' + id + '">Hide Applications</a>')
 });
+// <a href="<%=opportunity_path(opportunity)%>" class= "load_applications" data-id="<%=opportunity.id%>">View Applicants</a>
 
 e.preventDefault();
 });
 
+$(".hide_application").on("click", function(e){
+  var id = $(this).data("id");
+  $.get("/applications/" + id + ".json")
+  .success(function(json) {
+var $div = $("#applicant-" + id)
+$div.html("") //empties the div
+});
+
+e.preventDefault();
+});
 
 
 // End of function
