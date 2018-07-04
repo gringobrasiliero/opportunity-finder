7f418333ae7ea7988229fe98d8d726b42dfe7b5b
Nolan Byrnes <nolanehs@gmail.com>
2018-07-04 15:36:53 -0400

    Added Needed Params

--- app/serializers/profile_serializer.rb
+++ app/serializers/profile_serializer.rb
@@ -1,3 +1,4 @@
 class ProfileSerializer < ActiveModel::Serializer
-  attributes :id,  :first_name, :last_name, :picture_url, :location
+  attributes :id,  :first_name, :last_name, :picture_url, :location, :email, :profession, :user_id
+    belongs_to :applications
 end
