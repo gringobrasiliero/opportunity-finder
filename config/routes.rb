4851da5b93a573001e9a51c723f21e360ccc75a2
Nolan Byrnes <nolanehs@gmail.com>
2018-07-04 15:35:50 -0400

    Deleted White Space

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ get 'users/:id/opportunities/application_data', to: 'opportunities#application_d
 get 'profiles/:id/edit', to: 'profiles#edit', as: :edit_profile
 get 'applications/:id/submitted', to: 'applications#submitted', as: :submitted_profile
 get 'opportunities/:id/opportunity_data', to: 'opportunities#opportunity_data'
+
 resources :profiles
 
 resources :applications
