c80c8eb3ec5e848ccd286e7c2491d45ae6d53d34
Nolan Byrnes <nolanehs@gmail.com>
2018-07-12 07:47:24 -0400

    Added function to render and submit new opp in opp#index

--- app/assets/javascripts/opportunities.js
+++ app/assets/javascripts/opportunities.js
@@ -17,7 +17,7 @@ Opportunity.prototype.renderDiv = function() {
 };
 
 $(function() {
-  $('form').click("submit", function(e) {
+  $('form').on("submit", function(e) {
     // debugger
     e.preventDefault();
     var $form = $(this);
@@ -81,8 +81,8 @@ var click_count = 0
 
 $(function () {
 var click_count = 0
-  $(".prev_opportunity").on("click", function (e){
     var id = $(this).data("id");
+    $(".prev_opportunity").on("click", function (e){
     var new_id = id - click_count
       $.get("/opportunities/" + new_id + ".json")
       .success(function(json) {
@@ -103,3 +103,54 @@ var click_count = 0
   })
   });
 });
+
+$(function() {
+  $('.fred').on("submit", function(e) {
+    // debugger
+    e.preventDefault();
+    var $form = $(this).find('form');
+
+    var action = $form.attr("action");
+
+    var params = $form.serialize();
+
+    $.ajax({
+      url: action,
+      data: params,
+      dataType: "json",
+      method: "POST"
+    })
+    .success(function(json){
+      var opportunity = new Opportunity(json);
+debugger
+      var opportunityDiv = opportunity.renderDiv()
+
+      console.log(json)
+      alert("New Opportunity Created!")
+      $("div.new_opportunity").append(`<h1><a href="/opportunities/${json["id"]}">${json["profile"]["business_name"]}</a></h1>`)
+      $("div.new_opportunity").append(`<h2>${json["title"]}</h2>`)
+      $("div.new_opportunity").append(`<h2>${json["description"]}</h2>`)
+      $("div.new_opportunity").append("<br>")
+          $("form")[0].reset();
+
+    })
+    .error(function(response){
+    console.log("Broken", response)
+    })
+
+  });
+
+
+})
+
+
+$(function () {
+$(".create_form_opp").on("click", function(e){
+  $.ajax({
+    url: this.href,
+    dataType: 'script'
+  })
+
+    e.preventDefault();
+  })
+})
