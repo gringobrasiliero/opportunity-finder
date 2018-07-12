0d416e44472304d3817300b200ca9c6942c3ec5d
Nolan Byrnes <nolanehs@gmail.com>
2018-07-12 07:50:34 -0400

    added opp_id

--- app/serializers/profiles_serializer.rb
+++ app/serializers/profiles_serializer.rb
@@ -1,4 +1,5 @@
 class ProfilesSerializer < ActiveModel::Serializer
-  attributes :id,  :first_name, :last_name, :picture_url, :location, :application_id, :email, :profession, :user_id
+  attributes :id,  :first_name, :last_name, :picture_url, :location, :application_id, :email, :profession, :user_id, :opportunity_id
     belongs_to :applications
+    belongs_to :opportunity
 end
