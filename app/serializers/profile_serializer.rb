901c46eda216698ea083b1ce55cc14044bf0f20d
Nolan Byrnes <nolanehs@gmail.com>
2018-07-12 07:48:12 -0400

    added business name to profile serializer

--- app/serializers/profile_serializer.rb
+++ app/serializers/profile_serializer.rb
@@ -1,4 +1,5 @@
 class ProfileSerializer < ActiveModel::Serializer
-  attributes :id,  :first_name, :last_name, :picture_url, :location, :email, :profession, :user_id
+  attributes :id,  :first_name, :last_name, :picture_url, :location, :email, :profession, :user_id, :business_name
     belongs_to :applications
+    belongs_to :opportunity
 end
